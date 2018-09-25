d1eaa6446ab3d675857770c83329cb5dbdb103d0
Luiggi <lgarciasaladokuhl@outlook.com>
2018-09-25 18:44:50 -0300

    Arrow Function

--- EstruturasDeControl/condicionales.js
+++ EstruturasDeControl/condicionales.js
@@ -1,3 +1,5 @@
+const MAYORIA_DE_EDAD = 18 //las constantes siempre van con MAYUSCULA Y _ como nomenclatura
+
 var luis = {
   nombre: 'Luis',
   apellido: 'Garcia',
@@ -35,11 +37,35 @@ function imprimirProfesiones(persona) {
 Desafio
 --> Escribir si una persona es mayor de edad o menor
 */
+function esMayorDeEdad(persona) {
+  return persona.edad>=MAYORIA_DE_EDAD
+}
+
+//Arrow Function!
+//const esMayorDe18 = persona => persona.edad >= MAYORIA_DE_EDAD esto es equivalente a lo de arriba
+const esMayorDe18 = ({edad}) => edad >= MAYORIA_DE_EDAD
 
 function imprimirMayodeEdad(persona) {
-  if (persona.edad>=18) {
+  if (esMayorDeEdad) {
     console.log(`${persona.nombre} es mayor de edad`);
   }else {
+    console.log(`${persona.nombre} es menor de edad`);
+  }
+}
+
+function permitirAcceso(persona) {
+  if (!esMayorDe18(persona)) {
+    console.log('ACCESO DENEGADO');
+  }
+}
+/*
+Desafio
+Escribir en Arrow Function => la negacion de mayor de esMayorDeEdad
+*/
+const MenorDe18 = ({edad}) => !esMayorDe18(edad)
+
+function EsMenorDe18(persona) {
+  if (MenorDe18) {
     console.log(`${persona.nombre} es menor de edad`);
   }
 }
