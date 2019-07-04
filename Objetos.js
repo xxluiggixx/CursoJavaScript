ad5d14fcdbd4cdabd249f30d3523ff1b9479543a
LuiggiCasa <lgarciasaladokuhl@outlook.com>
2019-07-04 19:31:17 -0300

    Objetos terminados

--- Objetos.js
+++ Objetos.js
@@ -1,8 +1,29 @@
-var persona = {
+//Crear Objeto
+const persona = {
   nombre: 'Luis',
   apellido: 'Garcia',
-  edad: 27
+  profesion: 'Diseñador grafico',
+  email: 'lgarciasaladokuhl@outlook.com',
+  edad: 27,
+  musica: ['Trance', 'Pop', 'Electronica'],
+  hogar: {
+    ciudad:'Salta',
+    pais: 'Argentina'
+  },
+  fechaNacimiento: function(){
+    return new Date().getFullYear() - this.edad;
+  }
 }
+
+console.log(persona.musica[0]);
+
+//Arreglo de OBjeto
+const autos = [
+  {modelo:'Mustang', motor: 6.0},
+  {modelo:'Saveiro', motor: 1.6},
+  {modelo:'Suran', motor: 1.6}
+]
+console.log(autos[0].modelo);
 
 function imprimirNombre({nombre, edad}) {
   console.log(`Hola, me llamo ${nombre.toUpperCase()} y tengo ${edad} años`);
@@ -10,7 +31,7 @@ function imprimirNombre({nombre, edad}) {
 
 imprimirNombre(persona)
 imprimirNombre({nombre:'Jose'})
-/* *********************************************************************************************/
+/**********************************************************************************************/
 function cumpleanos(persona) {
   return{
     ...persona,
